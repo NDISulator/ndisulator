90aecc98f95743adb6dc54be21d0137dcacc2845
Paul B Mahol <onemda@gmail.com>
2009-11-09 00:00:50 +0000

    Do not check ndis_80211 twice.
    vap will be initialized only if ndis_80211 is not 0.

--- src/sys/dev/if_ndis/if_ndis.c
+++ src/sys/dev/if_ndis/if_ndis.c
@@ -1494,7 +1494,7 @@ ndis_inputtask(dobj, arg)
 		if (m == NULL)
 			break;
 		KeReleaseSpinLock(&sc->ndis_rxlock, irql);
-		if ((sc->ndis_80211 != 0) && (vap != NULL))
+		if (vap != NULL)
 			vap->iv_deliver_data(vap, vap->iv_bss, m);
 		else
 			(*ifp->if_input)(ifp, m);
@@ -1694,7 +1694,7 @@ ndis_ticktask(d, xsc)
 	    sc->ndis_sts == NDIS_STATUS_MEDIA_CONNECT) {
 		sc->ndis_link = 1;
 		NDIS_UNLOCK(sc);
-		if ((sc->ndis_80211 != 0) && (vap != NULL)) {
+		if (vap != NULL) {
 			ndis_getstate_80211(sc);
 			ieee80211_new_state(vap, IEEE80211_S_RUN, -1);
 		}
@@ -1706,7 +1706,7 @@ ndis_ticktask(d, xsc)
 	    sc->ndis_sts == NDIS_STATUS_MEDIA_DISCONNECT) {
 		sc->ndis_link = 0;
 		NDIS_UNLOCK(sc);
-		if ((sc->ndis_80211 != 0) && (vap != NULL))
+		if (vap != NULL)
 			ieee80211_new_state(vap, IEEE80211_S_SCAN, 0);
 		NDIS_LOCK(sc);
 		if_link_state_change(sc->ifp, LINK_STATE_DOWN);
