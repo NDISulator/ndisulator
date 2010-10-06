bd3fb1682f99be2b0e4377c6ac607189a84d50be
Paul B Mahol <onemda@gmail.com>
2010-10-06 21:20:33 +0000

    Use newly available feature on CURRENT.

--- src/sys/dev/if_ndis/if_ndis.c
+++ src/sys/dev/if_ndis/if_ndis.c
@@ -805,8 +805,8 @@ ndis_attach(device_t dev)
 	ifp->if_start = ndis_start;
 	ifp->if_init = ndis_init;
 	ifp->if_baudrate = 10000000;
-	IFQ_SET_MAXLEN(&ifp->if_snd, IFQ_MAXLEN);
-	ifp->if_snd.ifq_drv_maxlen = IFQ_MAXLEN;
+	IFQ_SET_MAXLEN(&ifp->if_snd, ifqmaxlen);
+	ifp->if_snd.ifq_drv_maxlen = ifqmaxlen;
 	IFQ_SET_READY(&ifp->if_snd);
 	ifp->if_capenable = ifp->if_capabilities;
 	ifp->if_hwassist = sc->ndis_hwassist;
