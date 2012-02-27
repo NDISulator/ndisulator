b056076044abbf3c0974599f4ca34a6107b14db3
Vladislav Movchan <vladislav.movchan@gmail.com>
2012-02-27 22:40:22 +0200

    Workaround a deadlock which could happen in KeAcquireSpinLockAtDpcLevel/KefAcquireSpinLockAtDpcLevel between "dpc" thread and one of "worker" threads/queues. In UP system case deadlock could happen if "worker" thread acquired a lock (which is not actual spin mutex in FreeBSD) and got preempted by "dpc" thread which is trying to acquire the same lock. "Dpc" thread has higher priority, so it will spin the lock forever without letting "worker" thread to finish its work and release the lock. In SMP system case this deadlock should not occur after sched_pin/sched_unpin got removed from KfRaiseIrql/KfLowerIrql functions.

--- src/sys/compat/ndis/subr_ntoskrnl.c
+++ src/sys/compat/ndis/subr_ntoskrnl.c
@@ -2056,12 +2056,18 @@ KefAcquireSpinLockAtDpcLevel(unsigned long *lock)
 {
 	while (atomic_cmpset_acq_int((volatile unsigned int *)lock, 0, 1) == 0)
 		/* sit and spin */;
+	thread_lock(curthread);
+	sched_prio(curthread, PRI_MIN_KERN);
+	thread_unlock(curthread);
 }
 
 void
 KefReleaseSpinLockFromDpcLevel(unsigned long *lock)
 {
 	atomic_store_rel_int((volatile unsigned int *)lock, 0);
+	thread_lock(curthread);
+	sched_prio(curthread, PRI_MIN_KERN + 20);
+	thread_unlock(curthread);
 }
 
 uint8_t
@@ -2080,12 +2086,18 @@ KeAcquireSpinLockAtDpcLevel(unsigned long *lock)
 {
 	while (atomic_cmpset_acq_int((volatile unsigned int *)lock, 0, 1) == 0)
 		/* sit and spin */;
+	thread_lock(curthread);
+	sched_prio(curthread, PRI_MIN_KERN);
+	thread_unlock(curthread);
 }
 
 void
 KeReleaseSpinLockFromDpcLevel(unsigned long *lock)
 {
 	atomic_store_rel_int((volatile unsigned int *)lock, 0);
+	thread_lock(curthread);
+	sched_prio(curthread, PRI_MIN_KERN + 20);
+	thread_unlock(curthread);
 }
 #endif /* __i386__ */
 
@@ -3526,7 +3538,7 @@ ntoskrnl_dpc_thread(void *arg)
 	 * once scheduled by an ISR.
 	 */
 	thread_lock(curthread);
-	sched_prio(curthread, PRI_MIN_KERN);
+	sched_prio(curthread, PRI_MIN_KERN + 20);
 	thread_unlock(curthread);
 
 	for (;;) {
